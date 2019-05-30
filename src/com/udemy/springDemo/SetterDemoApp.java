890361589bd0c6ba41ddc659faae29131621f8ae
n8bc0der <akki29911@gmail.com>
2019-05-30 21:31:22 +0530

    SetterDemoApp.java added & changes to applicationContextxml and FootballCoach.java

---
+++ src/com/udemy/springDemo/SetterDemoApp.java
@@ -0,0 +1,28 @@
+package com.udemy.springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//create container for the class/application
+		ClassPathXmlApplicationContext context = new 
+				ClassPathXmlApplicationContext("applicationContext.xml");
+
+		//Retrieve the bean from the container
+		FootballCoach footballCoach = context.getBean("myFootballCoach",FootballCoach.class);
+		
+		//Call application specific method required for the functionality
+		System.out.println(footballCoach.getDailyWorkout());
+		
+		System.out.println(footballCoach.getDailyFortune());
+		
+		//close the container
+		context.close();
+		
+		
+	}
+
+}
